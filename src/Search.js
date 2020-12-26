c899e36b66ba2935d039adb219bbe1a2c4aab230
zarap93 <70103108+zarap93@users.noreply.github.com>
2020-12-26 14:47:21 -0500

    Axios call for data
    For city, wind, humidity, temp and description

--- src/Search.js
+++ src/Search.js
@@ -5,73 +5,96 @@ import "./index.css";
 import { get } from "jquery";
 
 export default function Search() {
-  let weatherData = {
+  let weatherImage = {
     imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
   };
-  const apiKey = `689e969de90a91f7c9389015a9661d89`;
-  let city = "Toronto";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  return (
-    <div className="Search">
-      <div className="card">
-        <div className="row">
-          <div className="col-9">
-            <form className="form-inline" id="enter-city">
-              <input
-                type="text"
-                placeholder="Enter a city"
-                className="form-control"
-                id="search-city"
-              />
-              <button type="submit" className="form-control btn-primary">
-                <i className="fas fa-search"></i>
-              </button>
-            </form>
-          </div>
-          <div className="col-3">
-            <div className="unit-temps">
-              <span className="units">
-                <a href="/" className="active" id="celsius">
-                  {" "}
-                  °C
-                </a>{" "}
-                |
-                <a href="/" className="fahrenheit">
-                  {" "}
-                  °F
-                </a>
-              </span>
+
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: Math.round(response.data.main.temp),
+      wind: Math.round(response.data.wind.speed),
+      humidity: Math.round(response.data.main.humidity),
+      city: response.data.name,
+      description: response.data.weather[0].main,
+    });
+
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Search">
+        <div className="card">
+          <div className="row">
+            <div className="col-9">
+              <form className="form-inline" id="enter-city">
+                <input
+                  type="text"
+                  placeholder="Enter a city"
+                  className="form-control"
+                  id="search-city"
+                />
+                <button type="submit" className="form-control btn-primary">
+                  <i className="fas fa-search"></i>
+                </button>
+              </form>
+            </div>
+            <div className="col-3">
+              <div className="unit-temps">
+                <span className="units">
+                  <a href="/" className="active" id="celsius">
+                    {" "}
+                    °C
+                  </a>{" "}
+                  |
+                  <a href="/" className="fahrenheit">
+                    {" "}
+                    °F
+                  </a>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        <h4>Toronto</h4>
-        <ul className="date-time-weather">
-          <li>Last Updated: Thursday 15:45</li>
-          <li>Snow</li>
-        </ul>
-        <div className="row current-info">
-          <h1 className="col-4">-2°</h1>
-          <div className="col-4">
-            <img src={weatherData.imgUrl} alt={weatherData.description} />
+          <h4>{weatherData.city}</h4>
+          <ul className="date-time-weather">
+            <li>Last Updated: Thursday 15:45</li>
+            <li>{weatherData.description}</li>
+          </ul>
+          <div className="row current-info">
+            <h1 className="col-4">{weatherData.temperature}°</h1>
+            <div className="col-4">
+              <img src={weatherImage.imgUrl} alt={weatherData.description} />
+            </div>
+            <div className="col-4">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind} km/h</li>
+              </ul>
+            </div>
+            <div className="row time-of-day" id="daily-forecast"></div>
           </div>
-          <div className="col-4">
-            <ul>
-              <li>Humidity: 30%</li>
-              <li>Wind: 10 km/h</li>
-            </ul>
-          </div>
-          <div className="row time-of-day" id="daily-forecast"></div>
         </div>
+        <p>
+          <a
+            href="https://github.com/zarap93/weather-react-app.git"
+            target="_new"
+          >
+            Open-source code
+          </a>{" "}
+          by Zara Paris
+        </p>
       </div>
-      <p>
-        <a
-          href="https://github.com/zarap93/weather-react-app.git"
-          target="_new"
-        >
-          Open-source code
-        </a>{" "}
-        by Zara Paris
-      </p>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = `689e969de90a91f7c9389015a9661d89`;
+    let city = "Toronto";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleResponse);
+
+    return "Loading...";
+  }
 }
